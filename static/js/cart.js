df857d3393852c78f22d7417a73d77fbd8910f92
dobao2410 <dohoanggiabao2410@gmail.com>
2025-06-26 19:45:37 +0700

    commit ngày 26 th 6

---
+++ static/js/cart.js
@@ -0,0 +1,8 @@
+(function($) {
+    $(".add-cart").click(function() {
+        alert("Thêm vào giỏ hàng")
+    })
+    $('#yeu_thich').click(function(){
+        alert('Thay Danh Bel')
+    })
+})(jQuery)
